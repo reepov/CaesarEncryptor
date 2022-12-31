90191678e403c97eb2f4f6db668d647498529a94
Mikhail Strakhovskiy <mix.reep@mail.ru>
2022-12-31 15:16:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+List<string> alphabet = new List<string>();
+var str = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя ";
+for (int i = 0; i < str.Length; i++)
+{
+    alphabet.Add(str[i].ToString());
+}
+Console.WriteLine("Введите шифруемое слово");
+var toEncrypt = Console.ReadLine().ToCharArray();
+Console.WriteLine("Введите длину сдвига для шифра Цезаря");
+var offset = int.Parse(Console.ReadLine());
+string result = "";
+for (int i = 0; i < toEncrypt.Length; i++)
+{
+    result += alphabet[(alphabet.IndexOf(toEncrypt[i].ToString()) + offset) % alphabet.Count].ToString();
+}
+Console.WriteLine("Зашифрованный результат:");
+Console.WriteLine(result);
+toEncrypt = result.ToCharArray();
+result = "";
+for (int i = 0; i < toEncrypt.Length; i++)
+{
+    result += alphabet[(alphabet.IndexOf(toEncrypt[i].ToString()) - offset + alphabet.Count) % alphabet.Count].ToString();
+}
+Console.WriteLine("Дешифрованный результат:");
+Console.WriteLine(result);
+Console.ReadKey();
